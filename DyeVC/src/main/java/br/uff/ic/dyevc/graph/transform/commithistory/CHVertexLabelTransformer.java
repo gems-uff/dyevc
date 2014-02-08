d4cad3735ae5665574bea19537c79819447b2458
Cristiano Cesário <ccesario@ic.uff.br>
2014-02-08 09:41:00 -0200

    Collapsed nodes now show the number of containing nodes. This closes #103.

--- DyeVC/src/main/java/br/uff/ic/dyevc/graph/transform/commithistory/CHVertexLabelTransformer.java
+++ DyeVC/src/main/java/br/uff/ic/dyevc/graph/transform/commithistory/CHVertexLabelTransformer.java
@@ -1,26 +1,50 @@
 package br.uff.ic.dyevc.graph.transform.commithistory;
 
+//~--- non-JDK imports --------------------------------------------------------
+
 import br.uff.ic.dyevc.model.CommitInfo;
+
+import edu.uci.ics.jung.graph.Graph;
+
 import org.apache.commons.collections15.Transformer;
 
 /**
- * Transforms a CommitInfo in a string to be used as the label of a vertex.
+ * Transforms a object from the commit history graph in a string to be used as the label of a vertex.
  *
  * @author Cristiano
  */
 public class CHVertexLabelTransformer implements Transformer<Object, String> {
-
     /**
-     * Returns a string to be used as the label
+     * <p>Returns a string to be used as a label.</p>
+     * <p>If the object is a CommitInfo, returns the toString output as its label.</p>
+     * <p>If the object is a Graph, returns the number of contained nodes as its label.</p>
      *
-     * @param ci Info to be labeled
-     * @return the info formatted as a label
+     * @param o object to be labeled
+     * @return the object formatted as a label
      */
     @Override
     public String transform(Object o) {
         if (o instanceof CommitInfo) {
             return o.toString();
         }
+
+        if (o instanceof Graph) {
+            return Integer.toString(getNodecount((Graph)o));
+        }
+
         return "";
+    }
+
+    private int getNodecount(Graph graph) {
+        int total = 0;
+        for (Object o : graph.getVertices()) {
+            if (o instanceof Graph) {
+                total += getNodecount((Graph)o);
+            } else {
+                total++;
+            }
+        }
+
+        return total;
     }
 }
